d7d3c4a29c9a92900195b5a461d0950ff35be060
NiloufarSheyd <155656698+NiloufarSheid@users.noreply.github.com>
2025-05-01 12:04:10 +0330

    sample

---
+++ src/header/Header.jsx
@@ -0,0 +1,124 @@
+import React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import InputBase from '@mui/material/InputBase';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+// Styled components for Search
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(2),
+    width: 'auto',
+  },
+}));
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+}));
+export default function Header() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const isMenuOpen = Boolean(anchorEl);
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+  const menuId = 'product-menu';
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: 'bottom',
+        horizontal: 'left',
+      }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'left',
+      }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>Product 1</MenuItem>
+      <MenuItem onClick={handleMenuClose}>Product 2</MenuItem>
+      <MenuItem onClick={handleMenuClose}>Product 3</MenuItem>
+    </Menu>
+  );
+  return (
+    <Box sx={{ flexGrow: 1}}>
+      <AppBar position="sticky" sx={{ backgroundColor: '#5b744b ', width: '100%' }}>
+        <Toolbar>
+          {/* Logo Image */}
+          <img src="public/logo.jpg" alt="Logo" style={{ height: '20px', marginRight: '16px' }} />
+          
+          <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
+            x company
+          </Typography>
+          {/* Search Bar */}
+          <Search>
+            <SearchIconWrapper>
+              {/* You can add a Search Icon here if needed */}
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
+          {/* Products Dropdown */}
+          <IconButton
+            size="large"
+            aria-label="products menu"
+            aria-controls={menuId}
+            aria-haspopup="true"
+            onClick={handleMenuOpen}
+            color="inherit"
+          >
+            <Typography variant="body1">Products</Typography>
+            <ArrowDropDownIcon />
+          </IconButton>
+          {renderMenu}
+          {/* User Account Icon */}
+          <IconButton
+            size="large"
+            aria-label="account of current user"
+            color="inherit"
+          >
+            <AccountCircle />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
